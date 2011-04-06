511f4ffc7e32cb0bdb4364a1e0b370710786171f
Nyx (Neal Orman) <nyx@lindenlab.com>
2011-04-06 17:22:05 -0400

    first pass at updating for vs-2010 library package structure

--- build-cmd.sh
+++ build-cmd.sh
@@ -26,79 +26,79 @@ case "$AUTOBUILD_PLATFORM" in
         
 		mkdir -p stage/libraries/i686-win32/lib/{debug,release}
 		cp "external-libs/boost/lib/vc8/libboost_filesystem-d.lib" \
-				"stage/libraries/i686-win32/lib/debug/libboost_filesystem-d.lib"
+				"stage/lib/debug/libboost_filesystem-d.lib"
 		cp "external-libs/boost/lib/vc8/libboost_system-d.lib" \
-				"stage/libraries/i686-win32/lib/debug/libboost_system-d.lib"
+				"stage/lib/debug/libboost_system-d.lib"
 		cp "build/vc8-1.4-d/libcollada14dom21-d.lib" \
-				"stage/libraries/i686-win32/lib/debug/libcollada14dom21-d.lib"
+				"stage/lib/debug/libcollada14dom21-d.lib"
 		cp "build/vc8-1.4-d/libcollada14dom21-d.dll" \
-				"stage/libraries/i686-win32/lib/debug/libcollada14dom21-d.dll"
+				"stage/lib/debug/libcollada14dom21-d.dll"
 				
 		
 
 		cp "external-libs/boost/lib/vc8/libboost_filesystem.lib" \
-				"stage/libraries/i686-win32/lib/release/libboost_filesystem.lib"
+				"stage/lib/release/libboost_filesystem.lib"
 		cp "external-libs/boost/lib/vc8/libboost_system.lib" \
-				"stage/libraries/i686-win32/lib/release/libboost_system.lib"
+				"stage/lib/release/libboost_system.lib"
 		cp "build/vc8-1.4/libcollada14dom21.lib" \
-				"stage/libraries/i686-win32/lib/release/libcollada14dom21.lib"
+				"stage/lib/release/libcollada14dom21.lib"
 		cp "build/vc8-1.4/libcollada14dom21.dll" \
-				"stage/libraries/i686-win32/lib/release/libcollada14dom21.dll"			
+				"stage/lib/release/libcollada14dom21.dll"			
         
     ;;
         "darwin")
-			libdir="$top/stage/libraries/universal-darwin/"
+			libdir="$top/stage"
             mkdir -p "$libdir"/lib_{debug,release}
 			make
 
 			cp "external-libs/boost/lib/mac/libboost_system.a" \
-				"$libdir/lib_debug/libboost_system.a"
+				"$libdir/lib/debug/libboost_system.a"
 			cp "external-libs/boost/lib/mac/libboost_filesystem.a" \
-				"$libdir/lib_debug/libboost_filesystem.a"
+				"$libdir/lib/debug/libboost_filesystem.a"
 			cp "build/mac-1.4-d/Collada14Dom-d.dylib" \
-				"$libdir/lib_debug/Collada4Dom-d.dylib"
+				"$libdir/lib/debug/Collada4Dom-d.dylib"
 
 			cp "external-libs/boost/lib/mac/libboost_system.a" \
-				"$libdir/lib_release/libboost_system.a"
+				"$libdir/lib/release/libboost_system.a"
 			cp "external-libs/boost/lib/mac/libboost_filesystem.a" \
-				"$libdir/lib_release/libboost_filesystem.a"
+				"$libdir/lib/release/libboost_filesystem.a"
 			cp "build/mac-1.4/Collada14Dom.dylib" \
-				"$libdir/lib_release/Collada4Dom.dylib"
+				"$libdir/lib/release/Collada4Dom.dylib"
 		;;
         "linux")
-			libdir="$top/stage/libraries/i686-linux/"
+			libdir="$top/stage"
             mkdir -p "$libdir"/lib_{debug,release}_client
 			make 
 
 			cp "external-libs/boost/lib/mingw/libboost_filesystem.a" \
-				"$libdir/lib_release_client/libboost_filesystem.a"
+				"$libdir/lib/release/libboost_filesystem.a"
 			cp "external-libs/boost/lib/mingw/libboost_system.a" \
-				"$libdir/lib_release_client/libboost_system.a"
+				"$libdir/lib/release/libboost_system.a"
 
 			cp "build/linux-1.4/libcollada14dom.so" \
-				"$libdir/lib_release_client/libcollada14dom.so"
+				"$libdir/lib/release/libcollada14dom.so"
 			cp "build/linux-1.4/libcollada14dom.so.2" \
-				"$libdir/lib_release_client/libcollada14dom.so.2"
+				"$libdir/lib/release/libcollada14dom.so.2"
 			cp "build/linux-1.4/libcollada14dom.so.2.1" \
-				"$libdir/lib_release_client/libcollada14dom.so.2.1"
+				"$libdir/lib/release/libcollada14dom.so.2.1"
 
 
 			cp "external-libs/boost/lib/mingw/libboost_filesystem.a" \
-				"$libdir/lib_debug_client/libboost_filesystem.a"
+				"$libdir/lib/debug/libboost_filesystem.a"
 			cp "external-libs/boost/lib/mingw/libboost_system.a" \
-				"$libdir/lib_debug_client/libboost_system.a"
+				"$libdir/lib/debug/libboost_system.a"
 
 			cp "build/linux-1.4-d/libcollada14dom-d.so" \
-				"$libdir/lib_debug_client/libcollada14dom-d.so"
+				"$libdir/lib/debug/libcollada14dom-d.so"
 			cp "build/linux-1.4-d/libcollada14dom-d.so.2" \
-				"$libdir/lib_debug_client/libcollada14dom-d.so.2"
+				"$libdir/lib/debug/libcollada14dom-d.so.2"
 			cp "build/linux-1.4-d/libcollada14dom-d.so.2.1" \
-				"$libdir/lib_debug_client/libcollada14dom-d.so.2.1"
+				"$libdir/lib/debug/libcollada14dom-d.so.2.1"
         ;;
 
 esac
-mkdir -p "stage/libraries/include/"
-cp -R "include" "stage/libraries/include/collada"
+mkdir -p "stage/include/collada"
+cp -R "include" "stage/include/collada"
 mkdir -p stage/LICENSES
 cp "license/scea-shared-source-lic1.0.txt" "stage/LICENSES/collada.txt"
 mkdir -p stage/LICENSES/collada-other
