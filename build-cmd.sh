b2cc5068c1340dc446a3a4ea42d71690eab5f6fa
Nicky Dasmijn <nicky.dasmijn@posteo.nl>
2022-10-01 11:57:24 +0200

    Replace -m$AUTOBUILD_ADDRSIZE with $AUTOBUILD_GCC_ARCH

--- build-cmd.sh
+++ build-cmd.sh
@@ -156,7 +156,7 @@ case "$AUTOBUILD_PLATFORM" in
 ##      fi
 
         # Default target per --address-size
-        opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+        opts="${TARGET_OPTS:-${AUTOBUILD_GCC_ARCH} $LL_BUILD_RELEASE}"
 
         # Handle any deliberate platform targeting
         if [ -z "${TARGET_CPPFLAGS:-}" ]; then
